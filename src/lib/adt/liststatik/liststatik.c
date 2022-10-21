8bac54c39f8291c3dead3e9830d8a3f5556d30ba
maikeljh <mjh191003@gmail.com>
2022-10-21 20:04:57 +0700

    setup tubes

---
+++ src/lib/adt/liststatik/liststatik.c
@@ -0,0 +1,421 @@
+/* File: liststatik.c */
+/* Body ADT List Statik */
+
+#include "liststatik.h"
+#include <stdio.h>
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create List kosong  */
+void CreateListStatik(ListStatik *l){
+    /* I.S. l sembarang */
+    /* F.S. Terbentuk List l kosong dengan kapasitas CAPACITY */
+    /* Proses: Inisialisasi semua elemen List l dengan MARK */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    IdxType idx;
+    for(idx = IDX_MIN; idx < CAPACITY; idx++) {
+        ELMT(*l, idx) = MARK;
+    }
+};
+
+
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int listLength(ListStatik l){
+    /* Mengirimkan banyaknya elemen efektif List */
+    /* Mengirimkan nol jika List kosong */
+    
+    /* KAMUS LOKAL */
+    int count = 0;
+
+    /* ALGORITMA */
+    while(ELMT(l, count) != MARK && count < CAPACITY){
+        count++;
+    }
+
+    return count;
+};
+
+
+/* *** Selektor INDEKS *** */
+IdxType getFirstIdx(ListStatik l){
+    /* Prekondisi : List l tidak kosong */
+    /* Mengirimkan indeks elemen l pertama */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    return IDX_MIN;
+};
+
+IdxType getLastIdx(ListStatik l){
+    /* Prekondisi : List l tidak kosong */
+    /* Mengirimkan indeks elemen l terakhir */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    return (listLength(l) - 1);
+};
+
+
+/* ********** Test Indeks yang valid ********** */
+boolean isIdxValid(ListStatik l, IdxType i){
+    /* Mengirimkan true jika i adalah indeks yang valid utk kapasitas List l */
+    /* yaitu antara indeks yang terdefinisi utk container*/
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    return ((i >= IDX_MIN) && (i < CAPACITY));
+};
+
+boolean isIdxEff(ListStatik l, IdxType i){
+    /* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
+    /* yaitu antara 0..length(l)-1 */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    return ((i >= IDX_MIN) && (i < listLength(l)));
+};
+
+
+/* ********** TEST KOSONG/PENUH ********** */
+/* *** Test List kosong *** */
+boolean isEmpty(ListStatik l){
+    /* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    return (listLength(l) == 0);
+};
+
+/* *** Test List penuh *** */
+boolean isFull(ListStatik l){
+    /* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    return (listLength(l) == CAPACITY);
+};
+
+
+/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* *** Mendefinisikan isi List dari pembacaan *** */
+void readList(ListStatik *l){
+    /* I.S. l sembarang */
+    /* F.S. List l terdefinisi */
+    /* Proses: membaca banyaknya elemen l dan mengisi nilainya */
+    /* 1. Baca banyaknya elemen diakhiri enter, misalnya n */
+    /*    Pembacaan diulangi sampai didapat n yang benar yaitu 0 <= n <= CAPACITY */
+    /*    Jika n tidak valid, tidak diberikan pesan kesalahan */
+    /* 2. Jika 0 < n <= CAPACITY; Lakukan n kali: 
+            Baca elemen mulai dari indeks 0 satu per satu diakhiri enter */
+    /*    Jika n = 0; hanya terbentuk List kosong */
+
+    /* KAMUS LOKAL */
+    int n;
+    IdxType idx;
+    ElType element;
+
+    /* ALGORITMA */
+    CreateListStatik(l);
+
+    while(true){
+        scanf("%d", &n);
+        if(n >= IDX_MIN && n <= CAPACITY){
+            break;
+        } else {
+            continue;
+        }
+    }
+
+    for(idx = 0; idx < n; idx++){
+        scanf("%d", &element);
+        ELMT(*l, idx) = element;
+    }
+
+};
+
+void printList(ListStatik l){
+    /* Proses : Menuliskan isi List dengan traversal, List ditulis di antara kurung 
+    siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+    karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+    /* I.S. l boleh kosong */
+    /* F.S. Jika l tidak kosong: [e1,e2,...,en] */
+    /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+    /* Jika List kosong : menulis [] */
+
+    /* KAMUS LOKAL */
+    IdxType idx;
+
+    /* ALGORITMA */
+    printf("[");
+
+    if(isEmpty(l)){
+        printf("]");
+    } else {
+        for(idx = getFirstIdx(l); idx <= getLastIdx(l); idx++){
+            if(idx != getLastIdx(l)){
+                printf("%d,", ELMT(l, idx));
+            } else {
+                printf("%d", ELMT(l, idx));
+            }
+        }
+        printf("]");
+    }
+};
+
+
+/* ********** OPERATOR ARITMATIKA ********** */
+/* *** Aritmatika List : Penjumlahan, pengurangan, perkalian, ... *** */
+ListStatik plusMinusList(ListStatik l1, ListStatik l2, boolean plus){
+    /* Prekondisi : l1 dan l2 berukuran sama dan tidak kosong */
+    /* Jika plus = true, mengirimkan  l1+l2, yaitu setiap elemen l1 dan l2 pada 
+        indeks yang sama dijumlahkan */
+    /* Jika plus = false, mengirimkan l1-l2, yaitu setiap elemen l1 dikurangi 
+        elemen l2 pada indeks yang sama */
+
+    /* KAMUS LOKAL */
+    IdxType idx;
+    ListStatik newList;
+
+    /* ALGORITMA */
+    CreateListStatik(&newList);
+    for(idx = IDX_MIN; idx <= getLastIdx(l1); idx++){
+        if(plus) {
+            ELMT(newList, idx) = ELMT(l1, idx) + ELMT(l2, idx);
+        } else {
+            ELMT(newList, idx) = ELMT(l1, idx) - ELMT(l2, idx);
+        }
+    }
+
+    return newList;
+};
+
+/* ********** OPERATOR RELASIONAL ********** */
+/* *** Operasi pembandingan List: *** */
+boolean isListEqual(ListStatik l1, ListStatik l2){
+    /* Mengirimkan true jika l1 sama dengan l2 yaitu jika ukuran l1 = l2 dan semua 
+    elemennya sama */
+
+    /* KAMUS LOKAL */
+    IdxType idx;
+    boolean equal = true;
+
+    /* ALGORITMA */
+    for(idx = IDX_MIN; idx <= getLastIdx(l1); idx++){
+        if(ELMT(l1, idx) != ELMT(l2, idx)){
+            equal = false;
+            break;
+        }
+    }
+
+    return equal;
+};
+
+
+/* ********** SEARCHING ********** */
+/* ***  Perhatian : List boleh kosong!! *** */
+int indexOf(ListStatik l, ElType val){
+    /* Search apakah ada elemen List l yang bernilai val */
+    /* Jika ada, menghasilkan indeks i terkecil, dengan ELMT(l,i) = val */
+    /* Jika tidak ada atau jika l kosong, mengirimkan IDX_UNDEF */
+    /* Skema Searching yang digunakan bebas */
+
+    /* KAMUS LOKAL */
+    IdxType idx;
+
+    /* ALGORITMA */
+    if(isEmpty(l)){
+        return IDX_UNDEF;
+    } else {
+        for(idx = IDX_MIN; idx <= getLastIdx(l); idx++){
+            if(ELMT(l, idx) == val){
+                return idx;
+            }
+        }
+
+        return IDX_UNDEF;
+    }
+};
+
+/* ********** NILAI EKSTREM ********** */
+void extremeValues(ListStatik l, ElType *max, ElType *min){
+    /* I.S. List l tidak kosong */
+    /* F.S. Max berisi nilai terbesar dalam l;
+            Min berisi nilai terkecil dalam l */
+    
+    /* KAMUS LOKAL */
+    IdxType idx;
+
+    /* ALGORITMA */
+    *max = ELMT(l, IDX_MIN);
+    *min = ELMT(l, IDX_MIN);
+
+    for(idx = IDX_MIN + 1; idx <= getLastIdx(l); idx++){
+        if(ELMT(l, idx) > *max){
+            *max = ELMT(l, idx);
+        }
+        if(ELMT(l, idx) < *min){
+            *min = ELMT(l, idx);
+        }
+    }
+};
+
+
+/* ********** MENAMBAH ELEMEN ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertFirst(ListStatik *l, ElType val){
+    /* Proses: Menambahkan val sebagai elemen pertama List */
+    /* I.S. List l boleh kosong, tetapi tidak penuh */
+    /* F.S. val adalah elemen pertama l yang baru */
+
+    /* KAMUS LOKAL */
+    IdxType idx;
+
+    /* ALGORITMA */
+    if(isEmpty(*l)){
+        ELMT(*l, IDX_MIN) = val;
+    } else {
+        for(idx = getLastIdx(*l) + 1; idx > IDX_MIN; idx--){
+            ELMT(*l, idx) = ELMT(*l, idx-1);
+        }
+        ELMT(*l, IDX_MIN) = val;
+    }
+};
+
+/* *** Menambahkan elemen pada index tertentu *** */
+void insertAt(ListStatik *l, ElType val, IdxType idx){
+    /* Proses: Menambahkan val sebagai elemen pada index idx List */
+    /* I.S. List l tidak kosong dan tidak penuh, idx merupakan index yang valid di l */
+    /* F.S. val adalah elemen yang disisipkan pada index idx l */
+
+    /* KAMUS LOKAL */
+    IdxType i;
+
+    /* ALGORITMA */
+    for(i = getLastIdx(*l) + 1; i > idx; i--){
+            ELMT(*l, i) = ELMT(*l, i-1);
+    }
+
+    ELMT(*l, idx) = val;
+};
+
+
+/* *** Menambahkan elemen terakhir *** */
+void insertLast(ListStatik *l, ElType val){
+    /* Proses: Menambahkan val sebagai elemen terakhir List */
+    /* I.S. List l boleh kosong, tetapi tidak penuh */
+    /* F.S. val adalah elemen terakhir l yang baru */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    if(isEmpty(*l)) {
+        ELMT(*l, IDX_MIN) = val;
+    } else {
+        ELMT(*l, getLastIdx(*l) + 1) = val;
+    }
+};
+
+
+/* ********** MENGHAPUS ELEMEN ********** */
+/* *** Menghapus elemen pertama *** */
+void deleteFirst(ListStatik *l, ElType *val){
+    /* Proses : Menghapus elemen pertama List */
+    /* I.S. List tidak kosong */
+    /* F.S. val adalah nilai elemen pertama l sebelum penghapusan, */
+    /*      Banyaknya elemen List berkurang satu */
+    /*      List l mungkin menjadi kosong */
+
+    /* KAMUS LOKAL */
+    IdxType idx;
+
+    /* ALGORITMA */
+    *val = ELMT(*l, IDX_MIN);
+
+    for(idx = IDX_MIN; idx < getLastIdx(*l); idx++){
+        ELMT(*l, idx) = ELMT(*l, idx + 1);
+    }
+
+    ELMT(*l, getLastIdx(*l)) = MARK;
+};
+
+/* *** Menghapus elemen pada index tertentu *** */
+void deleteAt(ListStatik *l, ElType *val, IdxType idx){
+    /* Proses : Menghapus elemen pada index idx List */
+    /* I.S. List tidak kosong, idx adalah index yang valid di l */
+    /* F.S. val adalah nilai elemen pada index idx l sebelum penghapusan, */
+    /*      Banyaknya elemen List berkurang satu */
+    /*      List l mungkin menjadi kosong */
+
+    /* KAMUS LOKAL */
+    IdxType i;
+
+    /* ALGORITMA */
+    *val = ELMT(*l, idx);
+
+    for(i = idx; i < getLastIdx(*l); i++){
+        ELMT(*l, i) = ELMT(*l, i + 1);
+    }
+    
+    ELMT(*l, getLastIdx(*l)) = MARK;
+};
+
+/* *** Menghapus elemen terakhir *** */
+void deleteLast(ListStatik *l, ElType *val){
+    /* Proses : Menghapus elemen terakhir List */
+    /* I.S. List tidak kosong */
+    /* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+    /*      Banyaknya elemen List berkurang satu */
+    /*      List l mungkin menjadi kosong */
+
+    /* KAMUS LOKAL */
+    /* ALGORITMA */
+    *val = ELMT(*l, getLastIdx(*l));
+    ELMT(*l, getLastIdx(*l)) = MARK;
+};
+
+
+/* ********** SORTING ********** */
+void sortList(ListStatik *l, boolean asc){
+    /* I.S. l boleh kosong */
+    /* F.S. Jika asc = true, l terurut membesar */
+    /*      Jika asc = false, l terurut mengecil */
+    /* Proses : Mengurutkan l dengan salah satu algoritma sorting,
+    algoritma bebas */
+
+    /* KAMUS LOKAL */
+    IdxType i, j, minimal, maksimal;
+    ElType tempElmt;
+
+    /* ALGORITMA */
+    if(asc){
+        for(i = IDX_MIN; i < getLastIdx(*l); i++){
+            minimal = i;
+
+            for(j = i + 1; j <= getLastIdx(*l); j++){
+                if(ELMT(*l, j) < ELMT(*l, minimal)){
+                    minimal = j;
+                }
+            }
+
+            tempElmt = ELMT(*l, i);
+            ELMT(*l, i) = ELMT(*l, minimal);
+            ELMT(*l, minimal) = tempElmt;
+        }
+    } else {
+        for(i = IDX_MIN; i < getLastIdx(*l); i++){
+            maksimal = i;
+
+            for(j = i + 1; j <= getLastIdx(*l); j++){
+                if(ELMT(*l, j) > ELMT(*l, maksimal)){
+                    maksimal = j;
+                }
+            }
+
+            tempElmt = ELMT(*l, i);
+            ELMT(*l, i) = ELMT(*l, maksimal);
+            ELMT(*l, maksimal) = tempElmt;
+        }
+    }
+};
+
---
+++ src/lib/adt/sederhana/point/point.c
@@ -0,0 +1,197 @@
+/* File: point.c */
+/* Body ADT POINT */
+
+#include "point.h"
+#include <math.h>
+        
+/* *** Konstruktor membentuk POINT *** */
+void CreatePoint (POINT * P, float X, float Y) {
+   /* Kamus Lokal */
+    /* Algoritma */
+   Absis(*P) = X;
+   Ordinat(*P) = Y;
+};
+/* Membentuk sebuah POINT dari komponen-komponennya */
+
+/* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */                                                 
+void BacaPOINT (POINT * P) {
+   /* Kamus Lokal */
+   float x, y;
+   /* Algoritma */
+   scanf("%f", &x);
+   scanf("%f", &y);
+   CreatePoint(P, x, y);
+}; 
+/* Membaca nilai absis dan ordinat dari keyboard dan membentuk 
+   POINT P berdasarkan dari nilai absis dan ordinat tersebut */
+/* Komponen X dan Y dibaca dalam 1 baris, dipisahkan 1 buah spasi */
+/* Contoh: 1 2 
+   akan membentuk POINT <1,2> */
+/* I.S. Sembarang */
+/* F.S. P terdefinisi */
+void TulisPOINT (POINT P){
+   /* Kamus Lokal */
+    /* Algoritma */
+   printf("(%.2f,%.2f)", Absis(P), Ordinat(P));
+};
+/* Nilai P ditulis ke layar dengan format "(X,Y)" 
+   tanpa spasi, enter, atau karakter lain di depan, belakang, 
+   atau di antaranya 
+   Output X dan Y harus dituliskan dalam bilangan riil dengan 2 angka di belakang koma.
+*/
+/* I.S. P terdefinisi */
+/* F.S. P tertulis di layar dengan format "(X,Y)" */                
+
+/* *** Kelompok operasi relasional terhadap POINT *** */
+boolean EQ (POINT P1, POINT P2){
+   /* Kamus Lokal */
+    /* Algoritma */
+   return ((Absis(P1) == Absis(P2)) && (Ordinat(P1) == Ordinat(P2)));
+};
+/* Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama */
+boolean NEQ (POINT P1, POINT P2){
+   /* Kamus Lokal */
+    /* Algoritma */
+   return ((Absis(P1) != Absis(P2)) || (Ordinat(P1) != Ordinat(P2)));
+};
+/* Mengirimkan true jika P1 tidak sama dengan P2 */
+
+/* *** Kelompok menentukan di mana P berada *** */
+boolean IsOrigin (POINT P){
+   /* Kamus Lokal */
+    /* Algoritma */
+   return ((Absis(P) == 0) && (Ordinat(P) == 0));
+};
+/* Menghasilkan true jika P adalah titik origin */
+boolean IsOnSbX (POINT P){
+   /* Kamus Lokal */
+    /* Algoritma */
+   return (Ordinat(P) == 0);
+};
+/* Menghasilkan true jika P terletak Pada sumbu X */
+boolean IsOnSbY (POINT P){
+   /* Kamus Lokal */
+    /* Algoritma */
+   return (Absis(P) == 0);
+};
+/* Menghasilkan true jika P terletak pada sumbu Y */
+int Kuadran (POINT P){
+   /* Kamus Lokal */
+    /* Algoritma */
+   if((Absis(P) > 0) && (Ordinat(P) > 0)) {
+      return 1;
+   } else if((Absis(P) < 0) && (Ordinat(P) > 0)) {
+      return 2;
+   } else if((Absis(P) < 0) && (Ordinat(P) < 0)) {
+      return 3;
+   } else {
+      return 4;
+   }
+};
+/* Menghasilkan kuadran dari P: 1, 2, 3, atau 4 */
+/* Prekondisi : P bukan titik origin, */
+/*              dan P tidak terletak di salah satu sumbu */
+
+/* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */                           
+POINT NextX (POINT P) {
+   /* Kamus Lokal */
+   POINT P2;
+   /* Algoritma */
+   CreatePoint(&P2, Absis(P) + 1, Ordinat(P));
+   return P2;
+};
+/* Mengirim salinan P dengan absis ditambah satu */              
+POINT NextY (POINT P) {
+   /* Kamus Lokal */
+   POINT P2;
+   /* Algoritma */
+   CreatePoint(&P2, Absis(P), Ordinat(P) + 1);
+   return P2;
+};
+/* Mengirim salinan P dengan ordinat ditambah satu */
+POINT PlusDelta (POINT P, float deltaX, float deltaY) {
+   /* Kamus Lokal */
+   POINT P2;
+   /* Algoritma */
+   CreatePoint(&P2, Absis(P) + deltaX, Ordinat(P) + deltaY);
+   return P2;
+};;
+/* Mengirim salinan P yang absisnya adalah Absis(P) + deltaX dan ordinatnya adalah Ordinat(P) + deltaY */
+POINT MirrorOf (POINT P, boolean SbX) {
+   /* Kamus Lokal */
+   POINT P2;
+   /* Algoritma */
+   if(SbX) {
+      CreatePoint(&P2, Absis(P), Ordinat(P) * (-1));
+   } else {
+      CreatePoint(&P2, Absis(P) * (-1), Ordinat(P));
+   }
+   return P2;
+};
+/* Menghasilkan salinan P yang dicerminkan terhadap salah satu sumbu */
+/* Jika SbX bernilai true, maka dicerminkan terhadap sumbu X */
+/* Jika SbX bernilai false, maka dicerminkan terhadap sumbu Y */
+float Jarak0 (POINT P){
+   /* Kamus Lokal */
+   float jarak;
+   /* Algoritma */
+   jarak = sqrt((pow(Absis(P),2)) + (pow(Ordinat(P),2)));
+   return jarak;
+}
+/* Menghitung jarak P ke (0,0) */
+float Panjang (POINT P1, POINT P2){
+   /* Kamus Lokal */
+   float panjang;
+   /* Algoritma */
+   panjang = sqrt((pow((Absis(P1) - Absis(P2)),2)) + (pow((Ordinat(P1) - Ordinat(P2)),2)));
+   return panjang;
+}
+/* Menghitung panjang garis yang dibentuk P1 dan P2 */
+/* Perhatikanlah bahwa di sini spec fungsi kurang baik sebab menyangkut ADT Garis. */
+/* Tuliskan spec fungsi yang lebih tepat. */
+void Geser (POINT *P, float deltaX, float deltaY) {
+   /* Kamus Lokal */
+    /* Algoritma */
+   CreatePoint(P, Absis(*P) + deltaX, Ordinat(*P) + deltaY);
+};
+/* I.S. P terdefinisi */
+/* F.S. P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY */
+void GeserKeSbX (POINT *P) {
+   /* Kamus Lokal */
+    /* Algoritma */
+   CreatePoint(P, Absis(*P), 0);
+};
+/* I.S. P terdefinisi */
+/* F.S. P berada pada sumbu X dengan absis sama dengan absis semula. */
+/* Proses : P digeser ke sumbu X. */
+/* Contoh : Jika koordinat semula (9,9), maka menjadi (9,0) */
+void GeserKeSbY (POINT *P){
+   /* Kamus Lokal */
+    /* Algoritma */
+   CreatePoint(P, 0, Ordinat(*P));
+};
+/* I.S. P terdefinisi*/
+/* F.S. P berada pada sumbu Y dengan ordinat yang sama dengan semula. */
+/* Proses : P digeser ke sumbu Y. */
+/* Contoh : Jika koordinat semula (9,9), maka menjadi (0,9) */
+void Mirror (POINT *P, boolean SbX) {
+   /* Kamus Lokal */
+    /* Algoritma */
+   if(SbX) {
+      CreatePoint(P, Absis(*P), Ordinat(*P) * (-1));
+   } else {
+      CreatePoint(P, Absis(*P) * (-1), Ordinat(*P));
+   }
+};
+/* I.S. P terdefinisi */
+/* F.S. P dicerminkan tergantung nilai SbX atau SbY */
+/* Jika SbX true maka dicerminkan terhadap sumbu X */
+/* Jika SbX false maka dicerminkan terhadap sumbu Y */
+void Putar (POINT *P, float Sudut) {
+   /* Kamus Lokal */
+   float radian = (-1) * Sudut / 57.2958;
+    /* Algoritma */
+   CreatePoint(P, (Absis(*P) * cos(radian)) - (Ordinat(*P) * sin(radian)), (Absis(*P) * sin(radian)) + (Ordinat(*P) * cos(radian)));
+};
+/* I.S. P terdefinisi */
+/* F.S. P digeser sebesar Sudut derajat dengan sumbu titik (0,0) */
