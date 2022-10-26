fda3779fe54c3ece0cf1e606a315f7cacb0f46ea
Febryan Arota <13521120@mahasiswa.itb.ac.id>
2022-10-26 21:39:04 +0700

    add charfilemachine & wordfilemachine

---
+++ src/lib/adt/wordfilemachine/charfilemachine.c
@@ -0,0 +1,31 @@
+#include "charfilemachine.h"
+#include <stdio.h>
+
+char currentCharFile;
+
+
+static FILE *file;
+static int retfile;
+
+void startCharFile(char fileName[])
+/* Mesin siap dioperasikan. File disiapkan untuk dibaca.
+   Karakter pertama yang ada pada file posisinya adalah pada jendela.
+   I.S. : sembarang
+   F.S. : currentCharFile adalah karakter pertama pada file */
+{
+  file = fopen(fileName, "r");
+  advCharFile();
+}
+
+void advCharFile()
+/* File dimajukan satu karakter.
+   I.S. : Karakter pada jendela = currentCharFile
+   F.S. : currentCharFile adalah karakter berikutnya dari currentCharFile yang lama */
+{
+  retfile = fscanf(file, "%c", &currentCharFile);
+}
+
+// int main (){
+//   startCharFile("test.txt");
+//   printf("%c", currentCharFile);
+// }
