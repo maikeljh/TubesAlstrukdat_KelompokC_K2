925e914659956d438c5d3c3b5e751a15b500cbb8
maikeljh <mjh191003@gmail.com>
2022-10-27 21:22:00 +0700

    fix main prioqueue

--- src/lib/adt/prioqueue/main_prioqueue.c
+++ src/lib/adt/prioqueue/main_prioqueue.c
@@ -10,21 +10,51 @@ int main(){
     Makanan M1, M2;
     int id, lokasi;
     TIME wk, lp;
-   // Word name;
+    Word NamaMakanan;
+    int idxNama = 0;
+    while(currentChar != '\n'){
+        for (int i = 0; i < currentWord.Length; i++){
+            NamaMakanan.TabWord[idxNama] = currentWord.TabWord[i];
+            idxNama++;
+        }
+        NamaMakanan.TabWord[idxNama] = ' ';
+        idxNama++;
+        ADVWORD();
+    }
+    for (int i = 0; i < currentWord.Length; i++){
+        NamaMakanan.TabWord[idxNama] = currentWord.TabWord[i];
+        idxNama++;
+    }
+    NamaMakanan.Length = idxNama;
     // Sampel data makanan (Data Bebek):
     id = 1; lokasi = 1; 
     CreateTime(&wk, 3, 2, 30);
     CreateTime(&lp, 0, 15, 0);
-    CreateMakanan(&M1, id, lokasi, currentWord, wk, lp);
+    CreateMakanan(&M1, id, lokasi, NamaMakanan, wk, lp);
     //
 
     // Data Sapi:
     printf("Nama makanan 2: \n");
     STARTWORD();
+    idxNama = 0;
+    while(currentChar != '\n'){
+        for (int i = 0; i < currentWord.Length; i++){
+            NamaMakanan.TabWord[idxNama] = currentWord.TabWord[i];
+            idxNama++;
+        }
+        NamaMakanan.TabWord[idxNama] = ' ';
+        idxNama++;
+        ADVWORD();
+    }
+    for (int i = 0; i < currentWord.Length; i++){
+        NamaMakanan.TabWord[idxNama] = currentWord.TabWord[i];
+        idxNama++;
+    }
+    NamaMakanan.Length = idxNama;
     id = 2; lokasi = 2;
     CreateTime(&wk, 0, 1, 10);
     CreateTime(&lp, 0, 10, 0);
-    CreateMakanan(&M2, id, lokasi, currentWord, wk, lp);
+    CreateMakanan(&M2, id, lokasi, NamaMakanan, wk, lp);
     //
 
     /* ALGORITMA UTAMA */
