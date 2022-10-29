9d1fad5e014d1dff08ead6ab9b341a3ba2da93e8
Kenneth Dave <listeringkids@gmail.com>
2022-10-29 19:07:03 +0700

    Penambahan fungsi pada ADT PrioQueueTime

--- src/lib/adt/prioqueue/prioqueuetime.h
+++ src/lib/adt/prioqueue/prioqueuetime.h
@@ -86,5 +86,13 @@ void PrintPrioQueueTime (PrioQueueTime Q);
 <time-n> <elemen-n>
 #
 */
+int CariMakanan(PrioQueueTime Q, Makanan X, int id);
+/* Mencari lokasi makanan (indeks) dalam inventory makanan dengan ID makanan*/
+
+void DequeueAt (PrioQueueTime *Q, Makanan * X, int idx);
+/* Proses: Menghapus X pada Q pada indeks ke- idx
+/* I.S. Q tidak mungkin kosong */
+/* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer;
+        Q mungkin kosong */
 
 #endif
